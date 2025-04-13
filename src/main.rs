cae5f81e84a89fe719379816e674601b9553b26b
Frederic-K <frederic.kreuder.pro@gmail.com>
2025-04-13 18:59:55 +0200

    set variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Rust learning: Guess my number game!");
+    println!("I'm thinking of a number between 1 and 25. Try to guess it!");
+
+    let max_attempts: u32 = 5;
+    println!("You have {} attempts to guess the number.", max_attempts);
+
+    let secret_number: u32 = rand::rng().random_range(..=25);
+    let mut attempts_taken: u32 = 0;
 }
